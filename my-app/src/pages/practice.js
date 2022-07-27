532a60bc7a02ff98987011f26a906783489b9cc1
celociousalltrin <celociousalltrinmf@gmail.com>
2022-07-27 17:34:16 +0530

    init

--- my-app/src/pages/practice.js
+++ my-app/src/pages/practice.js
@@ -1,14 +1,3 @@
-import { Data } from "./child";
-import { DiningMockData } from "../mockdata";
-import React from "react"
-
-class Name extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>d dfvesfvesves</h1>
-            </div>
-        )
-    }
+const Name = () = {
+    
 }
-export default Name;
