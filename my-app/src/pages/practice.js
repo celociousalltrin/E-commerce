9ad5fa9b8c6ff68d3f45c08809a2cc3541fad574
celociousalltrin <celociousalltrinmf@gmail.com>
2022-07-27 19:41:46 +0530

    init

--- my-app/src/pages/practice.js
+++ my-app/src/pages/practice.js
@@ -1,3 +1,29 @@
-const Name = () = {
-    
+import {useState} from "react"
+
+const Name = () =>{
+const [log,setLog] = useState(true)
+
+const handleClick = ()=>{
+    setLog(!log)
 }
+
+const In = () =>{
+    return (<div><button className="btn btn-primary"
+    onClick={handleClick}>Login</button>
+    <h4>Welcom Guest</h4></div>
+)}
+
+const Out = () =>{
+    return <button className="btn btn-secondary"
+    onClick={handleClick}>Log out</button>
+}
+    
+    return (
+        <div>
+            <h1>Hello</h1>
+            {log ? <In /> : <Out />}
+        </div>
+    )
+}
+
+export default Name;
