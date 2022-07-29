d082f5ea0461f98c8e175f126f5cfcd8171e7367
celociousalltrin <celociousalltrinmf@gmail.com>
2022-07-29 16:31:34 +0530

    init

--- my-app/src/pages/child.js
+++ my-app/src/pages/child.js
@@ -1,20 +1,12 @@
-export const Data = (Arr) =>{
-    const {display_name,avg_selling_price,avg_mrp,avg_discount,seller_names} = Arr
+export const Player = (w) =>{
+    const {country,name}=w
     return(
-        <div>
-        <h1 className="font-weight-bold">This is {display_name}</h1>
-        <h2>This is {avg_selling_price}</h2>
-        <h2>This is {avg_mrp}</h2>
-        <h2>This is {avg_discount}</h2>
-        <h2>This is {seller_names}</h2>
-        </div>
+        <h1>The name is {country} {name}</h1>
     )
 }
 
-Data.defaultProps = {
-    display_name:"No Data Found",
-    avg_selling_price:"No Data Found",
-    avg_mrp:"No Data Found",
-    avg_discount:"No Data Found",
-    seller_names:"No Data Found",
+Player.defaultProps = {
+    name:"vijay",
+    country:"TamilNadu"
+
 }
--- my-app/src/pages/practice.js
+++ my-app/src/pages/practice.js
@@ -1,7 +1,18 @@
-const Name = ()=>{
+import {useState,useEffect} from "react"
+
+const Name = () =>{
+    const [count,setCount]=useState(0)
+    const [cal,setCal] = useState(-2)
+    useEffect(()=>{
+        setCal(cal+2)
+    },[count])
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="mt-5 ml-5">
+            <h3>count: {count}</h3>
+    <button className="btn btn-sm btn-secondary" onClick={()=>setCount(count+1)}>
+        +
+    </button>
+    <h3>calculation:{cal}</h3>
         </div>
     )
 }
