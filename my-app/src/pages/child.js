f776f6ba221aab6f540434f47cfc892815b6b502
celociousalltrin <celociousalltrinmf@gmail.com>
2022-08-12 23:09:34 +0530

    init

--- my-app/src/pages/child.js
+++ my-app/src/pages/child.js
@@ -1,15 +1,13 @@
 
 
-const Child = ((Data)=>{
-    const {avg_mrp,avg_selling_price} = Data
+const Child = ({display_name,product_code})=>{
     return (
         <div>
-        <h1>
-   The actual mrp is {avg_mrp}
-        </h1>
-        <h2>The discount price is {avg_selling_price}</h2>
+            <h1>The display name is {display_name}</h1>
+            <h1>The display name is {product_code}</h1>
+
         </div>
     )
-}) 
+}
 
 export default Child;
--- my-app/src/pages/practice.js
+++ my-app/src/pages/practice.js
@@ -1,26 +1,14 @@
-import React from "react";
-import { useState } from "react";
-
+import {DiningMockData} from "../mockdata/index"
+import Child from "./child"
 const Name = ()=>{
-    const [show,setShow] = useState(true)
-
-    const hanldeClick = (()=>{
-        setShow(!show)
-    })
-    return (
-        <div className="ml-5 mt-5">
-            {show ? (<div>
-                <button className="btn btn-primary" onClick={hanldeClick}>
-        click here
-       </button>
-            </div>) : (
-                 <button className="btn btn-success" onClick={hanldeClick}>
-                 click 
-                </button>)}
-            
-      
-   </div>
-    )
+  const renderIteration = (()=>{
+    return DiningMockData.map((item)=><Child key={item.product_code} {...item}></Child>)
+  })
+  return (
+    <div>
+      {renderIteration()}
+    </div>
+  )
 }
 
 export default Name;
